cdc052f2176537efd4d65cddfe131f28388b7386
joallisson <joallissonnc@gmail.com>
2025-11-02 11:32:52 -0300

    Renderização condicional com IF

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,16 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-calculator',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './calculator.component.html',
   styleUrl: './calculator.component.scss'
 })
 export class CalculatorComponent {
   numero1: number = 0;
   numero2: number = 0;
-  resultado: number = 0;
+  resultado: number|null = null;
 
   calcResult(){
     this.resultado = this.numero1 + this.numero2;
