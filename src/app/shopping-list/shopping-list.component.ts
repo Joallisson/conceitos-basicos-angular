7c1058ad030227d1885320e4d0020d7616de4453
joallisson <joallissonnc@gmail.com>
2025-11-02 18:00:20 -0300

    Formulario de adição de itens

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-list',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './shopping-list.component.html',
   styleUrl: './shopping-list.component.scss'
 })
 export class ShoppingListComponent {
+  item: string = '';
 
+  addItem(){
+    console.log('Item adicionado: ' + this.item)
+    this.item = '';
+  }
 }
