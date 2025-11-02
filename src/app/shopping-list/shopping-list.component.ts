06f8ab92598774fca84e4cf78da3b3c7fb5d49b9
joallisson <joallissonnc@gmail.com>
2025-11-02 18:16:00 -0300

    Trabalhando com classe de modelo e Arrays

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,17 +1,27 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ItemList } from './item-list';
 
 @Component({
   selector: 'app-shopping-list',
-  imports: [FormsModule],
+  imports: [
+    FormsModule,
+  ],
   templateUrl: './shopping-list.component.html',
   styleUrl: './shopping-list.component.scss'
 })
 export class ShoppingListComponent {
   item: string = '';
+  list: ItemList[] = [];
 
   addItem(){
-    console.log('Item adicionado: ' + this.item)
+    let itemList = new ItemList();
+    itemList.name = this.item;
+    itemList.id = this.list.length + 1;
+    this.list.push(itemList)
+
     this.item = '';
+
+    console.table(this.list)
   }
 }
